eab0bdce95d9f3b42bd6584297674214efb12e28
Adam <adfr06001@utb.vaxjo.se>
2023-08-30 10:42:14 +0200

    Coin manager

---
+++ New Project/Assets/Coin maneger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coinmaneger : MonoBehaviour
+{
+    public GameObject prefab;
+    public GameObject coin;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(coin == null){
+        SpawnCoin();
+
+       } 
+    }
+
+    private void SpawnCoin(){
+        float x = Random.Range(-10f,10f);
+        float z = Random.Range(-10f, 10f); 
+        Vector3 position = new Vector3(x,1.5f,z );
+
+        coin = Instantiate(prefab);
+        coin.transform.position = position;
+    }
+}
